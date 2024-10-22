ec238dc62696e92b43750cb5197848d0df8a0387
Dhafa <sucsespart1@gmail.com>
2024-10-22 11:37:40 +0700

    edit

--- lib/hal_komputer.dart
+++ lib/hal_komputer.dart
@@ -17,7 +17,8 @@ class Komputer extends StatelessWidget {
             Padding(
               padding: EdgeInsets.all(20.0),
             ),
-            Image.asset(          //mengambil gambar menggunakan asset yangg di simpan di dalam folder project
+            Image.asset(
+              //mengambil gambar menggunakan asset yangg di simpan di dalam folder project
               "img/komputer.png",
               width: 200.0,
             ),
